f6d64f60fb1ada033a7eba557c8a7e19de3ec68c
AndreiAvram99 <53441456+AndreiAvram99@users.noreply.github.com>
2020-03-12 02:09:00 +0200

    v3.1

--- TW_PROIECT/JS/rankingResponsiveScript.js
+++ TW_PROIECT/JS/rankingResponsiveScript.js
@@ -1,3 +1,15 @@
+window.onload = function (){
+    document.getElementById("sidePanel").style.height = window.screen.height + "px";
+    console.log(window.screen.height);
+    console.log(document.getElementById("sidePanel").style.height);
+}
+
+window.onresize = function (){
+    document.getElementById("sidePanel").style.height = window.screen.height + "px";
+    console.log(window.screen.height);
+    console.log(document.getElementById("sidePanel").style.height);
+}
+
 function openPanel(event) {
     document.getElementById("sidePanel").style.width = "90%";
     document.getElementById("panelOpenBtn").style.display = "none";
